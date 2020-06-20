9d6139a86416b766abfda9a18c74fc9a7a062b8b
ChampionWin <championwillwin6@outlook.com>
2020-06-20 18:08:02 +0300

    added mysql server

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ var things = require('./things.js');
 //both index.js and things.js should be in same directory
 app.use('/things', things);
 
-app.listen(3000);
+app.use('/things', function(req, res, next){
+   console.log("A request for things received at " + Date.now());
+   next();
+});
+
+// Route handler that sends the response
+app.get('/things', function(req, res){
+   res.send('Things');
+});
+
+app.listen(4000);
--- things.js
+++ things.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.send('GET route on things');
+    res.send('GET route on things' + req.params.name);
 });
 router.post('/', function(req, res){
     res.send('POST  route on things');
